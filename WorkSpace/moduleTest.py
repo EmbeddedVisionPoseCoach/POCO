cbd101f8c91dc3bc3d523f98ec7c0a9ded9dde2c
Dongmin <kozx13@gmail.com>
2026-05-07 09:48:56 +0900

    TestPyUpdate

--- WorkSpace/moduleTest.py
+++ WorkSpace/moduleTest.py
@@ -11,3 +11,13 @@ print(f"MediaPipe: {mp.__version__}")
 print(f"Pandas: {pd.__version__}")
 print(f"JAX: {jax.__version__}")
 print("----------------------")
+
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+app = QApplication(sys.argv)
+w = QWidget()
+w.setWindowTitle('Qt5 윈도우 설치 확인')
+w.show()
+sys.exit(app.exec_())
