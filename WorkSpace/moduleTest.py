227395caba1f6b54deda4294dc19635fc33923d6
Dongmin <kozx13@gmail.com>
2026-05-07 16:01:35 +0900

    fix

--- WorkSpace/moduleTest.py
+++ WorkSpace/moduleTest.py
@@ -12,12 +12,11 @@ print(f"Pandas: {pd.__version__}")
 print(f"JAX: {jax.__version__}")
 print("----------------------")
 
+# import sys
+# from PyQt5.QtWidgets import QApplication, QWidget
 
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-
-app = QApplication(sys.argv)
-w = QWidget()
-w.setWindowTitle('Qt5 윈도우 설치 확인')
-w.show()
-sys.exit(app.exec_())
+# app = QApplication(sys.argv)
+# w = QWidget()
+# w.setWindowTitle('Qt5 윈도우 설치 확인')
+# w.show()
+# sys.exit(app.exec_())
