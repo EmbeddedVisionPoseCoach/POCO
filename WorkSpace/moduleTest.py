3b439954598b52e44f84f179c3711db3d11ddf67
Dongmin <kozx13@gmail.com>
2026-05-07 09:38:58 +0900

    moduleTest

---
+++ WorkSpace/moduleTest.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+import cv2
+import mediapipe as mp
+import pandas as pd
+import jax
+
+print("--- 설치 확인 결과 ---")
+print(f"TensorFlow: {tf.__version__}")
+print(f"OpenCV: {cv2.__version__}")
+print(f"MediaPipe: {mp.__version__}")
+print(f"Pandas: {pd.__version__}")
+print(f"JAX: {jax.__version__}")
+print("----------------------")
